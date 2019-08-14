4f20adec7e378bf72341f68af9be17bb7c632b42
koji Ishimoto <koji.programs@gmail.com>
2019-08-15 00:34:58 +0900

    Add theme-test to test npm module gatsby theme

---
+++ theme-test/gatsby-config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  plugins: [
+    '@dowdiness/gatsby-theme-events'
+  ]
+}
---
+++ theme-test/src/@dowdiness/gatsby-theme-events/theme.js
@@ -0,0 +1,9 @@
+import { theme as baseTheme } from '@dowdiness/gatsby-theme-events/src/theme'
+
+export const theme = {
+  ...baseTheme,
+  colors: {
+    ...baseTheme.colors,
+    primary: 'blue'
+  }
+}
